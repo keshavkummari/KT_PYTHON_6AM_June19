6b43a81b0369c6f06fe81f6242995990dba8170e
keshavkummari <keshav.kummari@gmail.com>
2019-06-10 06:56:02 +0530

    Overview

---
+++ File_And_Dir_Mgmt.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+"""
+Modules
+
+Search
+
+Download
+
+Install
+
+pip : Python Package Installer
+
+pip search os
+
+pip download os
+
+pip install os
+
+"""
+import os
+
+print(os.getcwd())
+
+# /Users/keshavkummari/KT_PYTHON_6AM_June19
+
+os.chdir("/Library")
+
+print(os.getcwd())
+
+print(os.listdir())
+
+os.chdir("/Users/keshavkummari/KT_PYTHON_6AM_June19")
+
+print(os.getcwd())
---
+++ Overview.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# Single Line : # ,' ' , " ", ''' ''' and """ """
+
+# Multi-line comments : ''' ''' and """ """
+
+'Rule of Creating Variables in Python'
+
+"""
+1. a-z
+2. A-Z
+3. a-zA-Z
+4. 0-9
+5. a-zA-Z0-9
+6. _
+7. a-zA-Z0-9_
+"""
+
+"Creating Variables in Python"
+
+a = 10          # Int
+
+B = 10.75       # Float
+
+aB = 3 + 5j     # Complex
+
+aB007 = 15
+
+# _index = '' " " ''' ''' or """ """
+
+firstname = 'Guido'
+middleName = "Van"
+Last_Name = '''Rossum'''
+
+email = """
+Hi Team,
+
+This is a Test Mail!
+
+Thanks,
+Keshav.
+"""
+
+print(a,type(a),id(a))
+print(B)
+print(aB)
+print(aB007)
+print(firstname)
+print(middleName)
+print(Last_Name)
+print(email)
