28e322b8883f47162e434c7b84af629fddc4e6ab
keshavkummari <keshav.kummari@gmail.com>
2019-06-12 06:57:37 +0530

    Variables

---
+++ Variables/Rule_Of_Creating_Variables.py
@@ -0,0 +1,77 @@
+#/usr/bin/python
+# A-Z     # Hash means single line comment
+# a-z
+# combinations of A-Z & a-z
+# 0-9
+# combinations of A-Z , a-z & 0-9
+# an underscore (_).
+# combinations of A-Z , a-z , 0-9 & _
+
+
+
+"""
+a = 1
+A = 2
+highScoreValue = 1
+HighScore = 2
+aVariable_check=10
+_id = 1020
+
+print(a,A,highScoreValue,HighScore,aVariable_check,_id)
+
+Data Types :
+
+Numbers
+Strings
+List
+Tuple
+Dict
+Set
+
+int()
+str()
+list()
+tuple()
+dict()
+set()
+"""
+
+# Single Line Comment
+
+'#! = Shebang'
+
+"/usr/bin/python = Absolute path of the python"
+
+# Multi-line comments
+
+"""
+Today we are discussing about
+Python.
+
+Python 2.x
+And
+Python 3.x 
+"""
+
+'''
+Today we are discussing about
+Python.
+
+Python 2.x
+And
+Python 3.x 
+'''
+
+#Python Code >> Compiler >> Python Byte Code >> Python Virtual Machine >> Machine Code
+
+# Numbers
+
+a = 10
+
+b = 10.75
+
+c = 3 + 4j
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
